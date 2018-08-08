aefdb32132c3a17047c4199abb3319e90eb15ccf
tok-kkk <yunshi@republicprotocol.com>
2018-08-08 15:35:39 +1000

    update dht bucket size

--- cmd/ingress/ingress.go
+++ cmd/ingress/ingress.go
@@ -69,7 +69,7 @@ func main() {
 		log.Fatalf("cannot create contract binder: %v", err)
 	}
 
-	dht := dht.NewDHT(multiAddr.Address(), 20)
+	dht := dht.NewDHT(multiAddr.Address(), 100)
 	swarmClient := grpc.NewSwarmClient(multiAddr)
 	swarmer := swarm.NewSwarmer(swarmClient, &dht)
 	orderbookClient := grpc.NewOrderbookClient()

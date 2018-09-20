911ee432e67a67749db4b8e9d71a82db481e0d7a
Vince Au <vinceau09@gmail.com>
2018-09-20 10:50:38 +1000

    Fix typo

--- cmd/ingress/ingress.go
+++ cmd/ingress/ingress.go
@@ -49,7 +49,7 @@ var approvedTraders = []string{
 func init() {
 	sentryDSN := os.Getenv("SENTRY_DSN")
 	if sentryDSN == "" {
-		log.Fatalln("Cannot find SENTRY_DSN environment variable. Exitting...")
+		log.Fatalln("Cannot find SENTRY_DSN environment variable. Exiting...")
 	}
 	raven.SetDSN(sentryDSN)
 }
